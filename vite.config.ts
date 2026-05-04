94f98338a1fa8a889aa216d1666f407baba224cc
oalishow <59035298+oalishow@users.noreply.github.com>
2026-05-04 10:10:23 -0300

    feat: Add PWA icons and favicon
    Includes PWA icons in multiple sizes and a favicon to improve application installation and browser tab display.

--- vite.config.ts
+++ vite.config.ts
@@ -91,6 +91,11 @@ export default defineConfig(({ mode }) => {
             start_url: '/',
             icons: [
               {
+                src: 'favicon.ico',
+                sizes: '64x64 32x32 24x24 16x16',
+                type: 'image/x-icon'
+              },
+              {
                 src: 'pwa-192x192.png',
                 sizes: '192x192',
                 type: 'image/png',
